e72aa0162498fae6c8ab234055bc36ce77375000
Mohammad Kazemi <kazemi_mcp@hotmail.com>
2021-08-09 05:36:09 +0430

    Remove unused imports

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import {Provider} from "react-redux";
-import store from "./reducer/store";
 
 ReactDOM.render(
   <React.StrictMode>
